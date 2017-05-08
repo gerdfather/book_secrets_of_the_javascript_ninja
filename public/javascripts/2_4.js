6944b008a2130a95cc9996a467494f234410ef6a
gerdfather <gerdfather@gmail.com>
2017-05-09 01:16:06 +0200

    Added Example 2_4 from the book.

---
+++ public/javascripts/2_4.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ghannebacher on 09.05.2017.
+ */
+
+function assert(value, desc){
+    var li = document.createElement("li");
+    li.className = value ? "pass" : "fail";
+    li.appendChild(document.createTextNode(desc));
+    document.getElementById("results").appendChild(li);
+}
+
+window.onload = function(){
+    assert(true, "The test suite is running.");
+    assert(false, "Fail!");
+};
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/2_4', function(req, res, next) {
+    res.render('2_4', { title: '2_4' });
+});
+
 module.exports = router;
